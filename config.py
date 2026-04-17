f809f230781513026277be702c36f418f95b18b1
zerodev6 <zerodev699@gmail.com>
2026-04-17 19:23:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,12 +8,9 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "your_api_hash")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
     MONGO_URL = os.environ.get("MONGO_URL", "your_mongodb_url")
-    ADMIN_ID = int(os.environ.get("ADMIN_ID", "12345678")) # Your ID
-    ADMIN_USERNAME = "@Venuboyy"
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", "12345678"))
     
-    # Force Subscribe Channels
     CHANNELS = ["zerodev2", "mvxyoffcail"]
-    
     PICS_URL = ["https://api.aniwallpaper.workers.dev/random?type=girl"]
-    
     LOG_FILE = "logs.txt"
+    SOURCE_LINK = "https://ouo.io/nBFMfH" # Update this
