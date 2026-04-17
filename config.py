41316050b7de86cb72e80f0ce5b37f29bfd341e4
zerodev6 <zerodev699@gmail.com>
2026-04-17 18:26:46 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    API_ID = int(os.environ.get("API_ID", "12345"))
+    API_HASH = os.environ.get("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "your_bot_token")
+    MONGO_URL = os.environ.get("MONGO_URL", "your_mongodb_url")
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", "12345678")) # Your ID
+    ADMIN_USERNAME = "@Venuboyy"
+    
+    # Force Subscribe Channels
+    CHANNELS = ["zerodev2", "mvxyoffcail"]
+    
+    PICS_URL = ["https://api.aniwallpaper.workers.dev/random?type=girl"]
+    
+    LOG_FILE = "logs.txt"
